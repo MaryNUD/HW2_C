5b167fe1b6602e42229452ef1f549efb6c9fe31c
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-05 19:38:16 +0300

    tried to make 02 task with third figure

---
+++ 02thirdF/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Insert a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 100 && n <= 999)
+{
+    int n3 = n % 10;
+    Console.WriteLine("The third figure is " + n3);
+}
+else 
+Console.WriteLine("There is no third figure or the number is too big "); 
+// I don't get how to make it work for longer numbers. I'm sorry!
