c9e79547403b2f6209eb60f1c4553dad50931c7c
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-05 19:18:36 +0300

    add 03 days of the week task

---
+++ 03weekends/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Insert a number of the day: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 1 && n <= 7)
+{
+    if (n == 6 || n == 7)
+        Console.WriteLine("Today is a weekend!!!");
+    else 
+        Console.WriteLine("Sorry, you have to go and work");
+}
+else 
+Console.WriteLine("There is no such day of the week");
